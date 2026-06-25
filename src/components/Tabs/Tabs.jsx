d5c61a667ea51a40d4be20e1cff9ce6a904027eb
Oksana Shtelma <oshtelma@gmail.com>
2026-06-25 20:35:24 +0300

    add task solution3

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,5 +1,7 @@
 export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
   const activeTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
+  const effectiveActiveId =
+    tabs.find(tab => tab.id === activeTabId)?.id || tabs[0].id;
 
   return (
     <div data-cy="TabsComponent">
@@ -7,14 +9,16 @@ export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
         <ul>
           {tabs.map(tab => (
             <li
-              className={tab.id === activeTabId ? 'is-active' : ''}
+              className={tab.id === effectiveActiveId ? 'is-active' : ''}
               key={tab.id}
               data-cy="Tab"
             >
               <a
                 href={`#${tab.id}`}
                 data-cy="TabLink"
-                onClick={() => activeTabId !== tab.id && onTabSelected(tab.id)}
+                onClick={() =>
+                  effectiveActiveId !== tab.id && onTabSelected(tab.id)
+                }
               >
                 {tab.title}
               </a>
